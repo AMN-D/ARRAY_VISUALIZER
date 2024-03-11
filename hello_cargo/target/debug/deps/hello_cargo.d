b456b81a0c7e9c81f6d4286d56290e76f636a26a
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-03-11 19:56:34 +0530

    learning cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+E:\ProgramData\Github\RUST\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+E:\ProgramData\Github\RUST\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+E:\ProgramData\Github\RUST\hello_cargo\target\debug\hello_cargo.exe: E:\ProgramData\Github\RUST\hello_cargo\src\main.rs
