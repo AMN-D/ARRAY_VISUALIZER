b27ff78740c86ad8b2a28b8db0b571d47d5b96ee
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-03-21 20:02:07 +0530

    line

--- src/array/array.js
+++ src/array/array.js
@@ -1,32 +1,22 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color:  0x00ff00,
-});
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera( 45, innerWidth/innerHeight, 1, 500 );
+camera.position.set(0, 0, 100);
 
-function animate() {
-    requestAnimationFrame( animate );
+camera.lookAt(0, 0, 0);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    renderer.render( scene, camera );
-}
+const scene = new THREE.Scene();
 
-if (renderer.capabilities.isWebGL2) {
-    animate();
-} else {
-    const warning = document.createElement('div');
-    warning.innerHTML = 'WebGL 2 is not available.';
-    document.getElementById('array-container').appendChild(warning);
-}
+const material = new THREE.LineBasicMaterial({ color: "grey" });
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+renderer.render( scene, camera );
