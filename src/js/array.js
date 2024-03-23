8d31e215d7e5f0d242ef9384532b5d6216e1230c
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-03-23 15:45:15 +0530

    donut

--- src/js/array.js
+++ src/js/array.js
@@ -10,6 +10,26 @@ const renderer = new THREE.WebGLRenderer({
 const bgTexture = new THREE.TextureLoader().load('https://i.postimg.cc/FH4sc3tS/background.jpg');
 scene.background = bgTexture;
 
+const glassblock = new THREE.TextureLoader().load('https://i.postimg.cc/zXKH9TTt/glass.png');
+const moonnormal = new THREE.TextureLoader().load('https://i.postimg.cc/fRTSqChx/lunarrock-n.png');
+const moontexture = new THREE.TextureLoader().load('https://i.postimg.cc/xTgcmmbq/lunarrock-s.png');
+
+const gblock = new THREE.Mesh(
+    new THREE.BoxGeometry(3,3,3),
+    new THREE.MeshStandardMaterial( {
+        map: glassblock,
+        transparent: true,  
+    } )
+);
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3,32,32),
+    new THREE.MeshStandardMaterial( {
+        map: moontexture,  
+        normalMap: moonnormal,
+    } )
+);
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
@@ -19,18 +39,20 @@ const material = new THREE.MeshStandardMaterial({
     color: 0xFF6347,
 });
 const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+scene.add( torus, gblock, moon );
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(0, 0, 0);
+pointLight.intensity = 200;
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
+ambientLight.intensity = 1.5;
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 
 const gridHelper = new THREE.GridHelper(200, 50);
 
-scene.add(pointLight, ambientLight, lightHelper, gridHelper);
+scene.add(pointLight, ambientLight );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
@@ -54,6 +76,10 @@ function animate() {
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.01;
     torus.rotation.z += 0.01;
+    
+    moon.rotation.x += 0.01;
+    moon.rotation.y += 0.01;
+    moon.rotation.z += 0.01;
 
     controls.update();
 
