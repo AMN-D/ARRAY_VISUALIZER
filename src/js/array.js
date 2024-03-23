b4bb532936bfa15464a5a423b4aa1a4e438219bd
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-03-23 20:10:15 +0530

    minecraft glass texture with arrays

--- src/js/array.js
+++ src/js/array.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const minecraftglass = "https://i.postimg.cc/KvxghRQm/glass.png";
+const minecraftglass = "https://mineblocks.com/1/wiki/images/1/15/Glass.png";
 
 const scene = new THREE.Scene();
 scene.fog = new THREE.Fog(0x121212, 2, 500);
@@ -27,13 +27,15 @@ scene.background = new THREE.Color(0x121212);
 directionalLight.castShadow = true; 
 
 // textures
+const mglass = textureLoader.load(minecraftglass);
 
 // models
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
     new THREE.MeshPhongMaterial({
-        color: 0xd6fd5d,
         flatShading: false,  
+        transparent: true,
+        map: mglass,
     })
 );
 
@@ -51,8 +53,7 @@ const floor = new THREE.Mesh(
 floor.rotation.x = -Math.PI / 2;
 floor.receiveShadow = true;
 
-// functions
-    
+
 // Positions
 camera.position.set(0, 10, 20);
 directionalLight.position.set(5, 10, 5);
@@ -62,6 +63,30 @@ directionalLight.position.set(5, 10, 5);
 const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 1); // Set the size of the helper
 
 scene.add( cube, ambientLight, directionalLight, floor, lightHelper );
+
+// functions
+function createCubes( positions ) {
+    const cubes = [];
+    positions.forEach((pos, index) => {
+        const cube = new THREE.Mesh(
+            new THREE.BoxGeometry(3, 3, 3),
+            new THREE.MeshPhongMaterial({
+                flatShading: false,  
+                map: mglass,
+                transparent: true,
+            })
+        );
+        cube.position.set(pos * 3, 0, 0);
+        cubes.push(cube);
+    });
+    return cubes;
+}
+
+const inputArray = [1, 2, 3, 4];
+const cubes = createCubes(inputArray);
+cubes.forEach(cube => {
+    scene.add(cube);
+});
 
 function animate() {
 	requestAnimationFrame( animate );
